c90b149d11dcb3979cece4a4e739addb3c6d6d32
Roman Potashow <justgook@gmail.com>
2012-12-21 12:00:28 +0200

    Starting building structure

---
+++ src/main/java/lv/z0/shaman/server/core/dataExchange.java
@@ -0,0 +1,11 @@
+package lv.z0.shaman.server.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gook
+ * Date: 12/21/12
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class dataExchange {
+}
---
+++ src/test/java/lv/z0/shaman/client/JavascriptHandlerTest.java
@@ -0,0 +1,14 @@
+package lv.z0.shaman.client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gook
+ * Date: 12/21/12
+ * Time: 11:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavascriptHandlerTest {
+    public void testHandle() throws Exception {
+
+    }
+}
